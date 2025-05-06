9f33555a2d0538c2b08cecfa4044468cd77a5eed
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-05-06 12:09:57 -0400

    Create auth middleware functions

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "./error.middleware";
+import { User as PrismaUser } from "../prisma/src/db";
+import passport from "passport";
+
+declare global {
+	namespace Express {
+		interface User extends PrismaUser {}
+	}
+}
+
+const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+	passport.authenticate(
+		"jwt",
+		{ session: false },
+		(err: Error, user: any, info: string) => {
+			if (err) next(err);
+
+			if (!user) throw new AppError(403, "User is not authenticated");
+
+			req.user = user;
+			next();
+		}
+	)(req, res, next);
+};
+
+const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+	if (req?.user?.role !== "ADMIN")
+		throw new AppError(403, "User is not authorized");
+
+	next();
+};
+
+export default { isAuthenticated, isAdmin };
