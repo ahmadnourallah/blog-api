c2fa9bfb3d184910aa2961c2ed66c524b37e56ae
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-05-06 12:10:13 -0400

    Update app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response } from "express";
+import authRouter from "./routes/auth.router";
+import errorHandler from "./middleware/error.middleware";
+import "./config/passport.config";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", authRouter);
+
+app.use(errorHandler);
 
 export default app;
