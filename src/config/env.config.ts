73110d7f0d410257ff45f316b2469a277998f1dc
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-05-06 12:04:34 -0400

    Add env variables for authentication

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -5,11 +5,15 @@ dotenv.config();
 interface ConfigType {
 	PORT: number;
 	DB_URL: string;
+	JWT_SECRET: string;
+	JWT_EXPIRATION_TIME: string;
 }
 
 const config: ConfigType = {
 	PORT: Number(process.env.PORT) || 3000,
-	DB_URL: process.env.DB_URL,
+	DB_URL: process.env.DB_URL || "",
+	JWT_SECRET: process.env.JWT_SECRET || "",
+	JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME || "2d",
 };
 
 export default config;
