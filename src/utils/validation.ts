7e03d0300b39b621019c902978c77e6ccaf0625c
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-05-07 13:29:37 -0400

    Add query filters

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,49 @@
 import { Request } from "express";
 import { AppError } from "../middleware/error.middleware";
-import { validationResult } from "express-validator";
+import { validationResult, query } from "express-validator";
+
+const validateQueries = () => [
+	query("start")
+		.default(0)
+		.toInt()
+		.isNumeric()
+		.withMessage("Start must be a number")
+		.isLength({ min: 0 })
+		.withMessage("Start cannot be negative")
+		.customSanitizer((start) => {
+			if (start > 0) return start - 1;
+			return start;
+		}),
+	query("end")
+		.default(10)
+		.toInt()
+		.isNumeric()
+		.withMessage("End must be a number")
+		.isLength({ min: 0 })
+		.withMessage("End cannot be negative")
+		.custom((end, { req }) => !(end <= Number(req?.query?.start)))
+		.withMessage("End must be larger than start")
+		.custom((end, { req }) => !(end - Number(req?.query?.start) >= 30))
+		.withMessage("Maximum number of items requested is 30"),
+	query("search")
+		.default("")
+		.trim()
+		.escape()
+		.isString()
+		.withMessage("Search must be a string"),
+	query("orderBy")
+		.default("date")
+		.trim()
+		.escape()
+		.custom((orderBy) => orderBy === "date" || orderBy === "title")
+		.withMessage("Order must be by title or date"),
+	query("order")
+		.default("asc")
+		.trim()
+		.escape()
+		.custom((order) => order === "asc" || order === "desc")
+		.withMessage("Order must be asc or desc"),
+];
 
 const validateResults = (req: Request) => {
 	const errors = validationResult(req);
@@ -15,4 +58,4 @@ const validateResults = (req: Request) => {
 		);
 };
 
-export { validateResults };
+export { validateResults, validateQueries };
