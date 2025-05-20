8be0dcd30828066e697931b5111f3e53f9a09917
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-05-20 09:44:37 -0400

    Tie app everything

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import authRouter from "./routes/auth.router";
-import errorHandler from "./middleware/error.middleware";
+import postRouter from "./routes/post.router";
+import categoryRouter from "./routes/category.router";
+import commentRouter from "./routes/comment.router";
+import userRouter from "./routes/user.router";
+import miscRouter from "./routes/misc.router";
+import {
+	serverErrorHandler,
+	clientErrorHandler,
+} from "./middleware/error.middleware";
 import "./config/passport.config";
 
 const app = express();
@@ -9,7 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRouter);
+app.use("/posts", postRouter);
+app.use("/categories", categoryRouter);
+app.use("/comments", commentRouter);
+app.use("/users", userRouter);
+app.use("/", miscRouter);
 
-app.use(errorHandler);
+app.use(clientErrorHandler);
+app.use(serverErrorHandler);
 
 export default app;
